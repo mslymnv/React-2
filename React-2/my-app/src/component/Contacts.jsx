89e14b7efd816029c321a4446f732ee29b429150
mslymnv <163085981+mslymnv@users.noreply.github.com>
2024-05-07 15:29:21 +0400

    Update Contacts.jsx

--- React-2/my-app/src/component/Contacts.jsx
+++ React-2/my-app/src/component/Contacts.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Contacts = () => {
   return (
     <div>
-        <p>Contacts: mirvarimuadova@gmail.com // 055 715 75 37</p>
+      <p>Contacts: memmedsuleymanov61@gmail.com // 055 123 45 67</p>
     </div>
-  )
-}
+  );
+};
 
-export default Contacts
+export default Contacts;
